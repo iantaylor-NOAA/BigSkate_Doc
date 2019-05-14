56f10911b1c709eea75d9f3179b718f77ed1e1ee
Ian Taylor <ian.taylor@noaa.gov>
2019-05-13 17:57:41 -0700

    add notes about replacing base model

---
+++ R/BigSkate_run_new_base_model.R
@@ -0,0 +1,16 @@
+# code related to running everything associated with a new base model
+
+# re-run profiles
+# code is in "BigSkate_profiles.R"          
+# can source the file and then copy block of commands for running the profiles
+
+# re-run retros
+# code is in "BigSkate_retro_and_jitter.R"
+# change runjitter = TRUE/FALSE within this file and then
+source('c:/SS/skates/BigSkate_Doc/R/BigSkate_retro_and_jitter.R')
+
+# re-run sensitivities
+# code is in "BigSkate_sensitivities.R"     
+# two options, one more automated than the other
+
+# re-run jitter
